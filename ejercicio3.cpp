4066704b916ce8832a274a375384982d134eda99
agustin de la sierra <a.delasierra01@ufromail.cl>
2020-12-09 18:44:16 -0300

    se añade ejercicio3

---
+++ ejercicio3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// variable global
+
+
+
+// codigo 
+int main (){
+ float numero1;
+ float numero2;
+ float suma;
+
+printf("ingrese el primer numero:");
+scanf("%f", &numero1);
+printf("ingrese el segundo numero:");
+scanf("%f", &numero2);
+suma = numero1 + numero2;
+printf("el rsultado de la suma es: %f", suma);
+
+
+  return 0;
+}
+
